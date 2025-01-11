c89f95d595ff37005768264d99e491f4cf80cd43
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2025-01-11 14:58:14 +0900

    feat: Deploying app using docker compose

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 git reset --hard
 git checkout master
 git pull origin master
-npm i
-pm2 start process.config.js --env production
+# npm i
+# pm2 start process.config.js --env production
+docker compose up -d
 
 # DEVELOPMENT
 # pm2 start process.config --env development
